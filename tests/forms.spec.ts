77b5bb31771da59ad21e3232a8057a9d0d1dadb2
Michael Naghavipour <39297270+MichaelNaghavipour@users.noreply.github.com>
2024-11-28 21:03:54 +0100

    added tests for form-layouts page (#1)
    Co-authored-by: Michael Naghavipour <m.naghavipour@reply.de>

--- tests/forms.spec.ts
+++ tests/forms.spec.ts
@@ -9,17 +9,66 @@ test.beforeEach( async ({ page }) => {
 test.describe('Form Layouts', () => {
 
     test('Inline form', async ({ page }) => {
-        const inlineFormCard = page.locator('nb-card', {hasText: 'Inline form'})
+        const inlineForm = page.locator('nb-card', {hasText: 'Inline form'})
+        await inlineForm.getByRole('textbox', {name: 'Jane Doe'}).fill('Michael Naghavipour')
+        await inlineForm.getByRole('textbox', {name: 'Email'}).fill('inlineform@test.com')
+        const inlineFormCheckbox = inlineForm.locator('nb-checkbox')
+        await inlineFormCheckbox.click()
+        await expect(inlineFormCheckbox.locator('.custom-checkbox-icon.ng-star-inserted')).toBeVisible()
+        expect(inlineForm.getByText('Remember me')).toBeVisible()
+        await inlineForm.getByRole('button', {name: 'Submit'}).click()
+    })
 
-        await inlineFormCard.getByRole('textbox', {name: 'Jane Doe'}).fill('Bruce Lee')
-        await inlineFormCard.getByRole('textbox', {name: 'Email'}).fill('inlineform@test.com')
+    test('Using the Grid', async ({ page }) => {
+        const usingTheGrid = page.locator('nb-card', {hasText: 'Using the Grid'})
+        await usingTheGrid.getByRole('textbox', {name: 'Email'}).fill('usingthegrid@test.com')
+        await usingTheGrid.getByRole('textbox', {name: 'Password'}).fill('test@password')
+        const radioButton = usingTheGrid.locator('nb-radio')
+        await radioButton.getByRole('radio', {name: 'Option 1'}).check({ force: true })
+        expect(await radioButton.getByRole('radio', {name: 'Option 1'}).isChecked()).toBeTruthy()
+        await radioButton.getByRole('radio', {name: 'Option 2'}).check({ force: true })
+        expect(await radioButton.getByRole('radio', {name: 'Option 2'}).isChecked()).toBeTruthy()
+        expect(await radioButton.getByRole('radio', {name: 'Option 1'}).isChecked()).toBeFalsy()
+        await usingTheGrid.getByRole('button', {name: 'Sign in'}).click()
+    })
 
-        const checkbox = inlineFormCard.locator('nb-checkbox')
-        await checkbox.click()
+    test('Basic form', async ({ page }) => {
+        const basicForm = page.locator('nb-card', {hasText: 'Basic form'})
+        await basicForm.getByRole('textbox', {name: 'Email'}).fill('basicform@test.com')
+        await basicForm.getByRole('textbox', {name: 'Password'}).fill('test@password')
+        const basicFormCheckbox = basicForm.locator('nb-checkbox')
+        await basicFormCheckbox.click()
+        await expect(basicFormCheckbox.locator('.custom-checkbox-icon.ng-star-inserted')).toBeVisible()
+        expect(basicForm.getByText('Check me out')).toBeVisible
+        await basicForm.getByRole('button', {name: 'Submit'}).click()
+    })
 
-        await expect(checkbox.locator('.custom-checkbox-icon.ng-star-inserted')).toBeVisible()
+    test('Form without labels', async ({ page }) => {
+        const formWithoutLabels = page.locator('nb-card', {hasText: 'Form without labels'})
+        await formWithoutLabels.getByRole('textbox', {name: 'Recipients'}).fill('Dear HR Manager')
+        await formWithoutLabels.getByRole('textbox', {name: 'Subject'}).fill('Test Automation Engineer Job Apllication')
+        await formWithoutLabels.getByRole('textbox', {name: 'Message'}).fill('You can hire me to achieve your business goals')
+        await formWithoutLabels.getByRole('button', {name: 'Send' }).click()
+    })
 
-        await inlineFormCard.getByRole('button', {name: 'Submit'}).click()
+    test('Block form', async ({ page }) => {
+        const blockForm = page.locator('nb-card', {hasText: 'Block form'})
+        await blockForm.getByRole('textbox', {name: 'First Name'}).fill('Michael')
+        await blockForm.getByRole('textbox', {name: 'Last Name'}).fill('Naghavipour')
+        await blockForm.getByRole('textbox', {name: 'Email'}).fill('blockform@test.com')
+        await blockForm.getByRole('textbox', {name: 'Website'}).fill('https://naghavipour.click/')
+        await blockForm.getByRole('button', {name: 'Submit'}).click()
+    })
+
+    test('Horizontal form', async ({ page }) => {
+        const horizontalForm = page.locator('nb-card', {hasText: 'Horizontal form'})
+        await horizontalForm.getByRole('textbox', {name: 'Email'}).fill('horizontalform@test.com')
+        await horizontalForm.getByRole('textbox', {name: 'Password'}).fill('test@password')
+        const horizontalFormCheckbox = horizontalForm.locator('nb-checkbox')
+        await horizontalFormCheckbox.click()
+        await expect(horizontalFormCheckbox.locator('.custom-checkbox-icon.ng-star-inserted')).toBeVisible()
+        expect(horizontalForm.getByText('Remember me')).toBeVisible()
+        await horizontalForm.getByRole('button', {name: 'Sign in'}).click()
     })
 })
 
